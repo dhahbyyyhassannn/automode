dff1ac2b604881b49e1405d7baadafa3212101ab
dhahbyyyhassannn <dhahbyyy.hassannn@gmail.com>
2026-04-26 16:32:05 +0100

    initialisation

---
+++ automodebackend/src/main/java/com/example/automodebackend/AutomodebackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.automodebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomodebackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutomodebackendApplication.class, args);
+    }
+
+}
---
+++ automodebackend/src/test/java/com/example/automodebackend/AutomodebackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.automodebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomodebackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
